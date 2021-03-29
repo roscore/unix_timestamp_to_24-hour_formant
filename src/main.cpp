615b26822472f8e41ce09aac7490c956e72a558c
roscore <roscore@kakao.com>
2021-03-29 18:07:43 +0900

    fixed cmake error

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,34 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "pub_time");
 	ros::NodeHandle nh;
 
-	ros::Publisher time_pub = nh.advertise<std_msgs::Int32>("sys_time", 10);
+	ros::Publisher unix_time_pub	= nh.advertise<std_msgs::Int32>("unix_time", 10);
+	ros::Publisher utc_time_pub	= nh.advertise<std_msgs::String>("utc_time", 10);
+	ros::Publisher kor_time_pub	= nh.advertise<std_msgs::String>("kor_time", 10);
 
 	ros::Rate loop_rate(100);
 
 	while(ros::ok())
 	{
-		std_msgs::Int32 msg;
-		
-		int now_time, now_hour, kor_hour;
+		std_msgs::Int32 unix_time_msg;
+		std_msgs::String utc_time_msg;
+		std_msgs::String kor_time_msg;
 
-		now_time = ros::Time::now().toSec();
-	        now_hour = now_time % 86400;
-		kor_hour = now_hour + 32400;	
+		int utc_time, utc_hour, kor_hour;
 
-		msg.data = kor_hour;
-		
-		if(msg.data > 43200)	ROS_INFO("PM || %d : %d : %d ", kor_hour / 3600, (kor_hour % 3600) / 60, kor_hour % 60);
+		utc_time = ros::Time::now().toSec();
+	        utc_hour = utc_time % 86400;
+		kor_hour = utc_hour + 32400;	
+
+		if(kor_hour > 43200)	ROS_INFO("PM || %d : %d : %d ", kor_hour / 3600, (kor_hour % 3600) / 60, kor_hour % 60);
 		else			ROS_INFO("AM || %d : %d : %d ", kor_hour / 3600, (kor_hour % 3600) / 60, kor_hour % 60);	
-		time_pub.publish(msg);	
+	
+		unix_time_msg.data = utc_time;
+		utc_time_msg.data = "%d : %d : %d" , utc_hour / 3600, (utc_hour % 3600) / 60, utc_hour % 60;
+		kor_time_msg.data = "%d : %d : %d" , kor_hour / 3600, (kor_hour % 3600) / 60, kor_hour % 60;	
+		
+		unix_time_pub.publish(unix_time_msg);
+		utc_time_pub.publish(utc_time_msg);
+		kor_time_pub.publish(kor_time_msg);	
 
 		ros::spinOnce();
 
